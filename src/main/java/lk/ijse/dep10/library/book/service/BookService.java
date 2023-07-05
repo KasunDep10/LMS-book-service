1be2b22d44e40140265ecd40ce7b528e218f1a81
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-05 14:15:48 +0530

    Add BookService interface to service layer

--- src/main/java/lk/ijse/dep10/library/book/service/BookService.java
+++ src/main/java/lk/ijse/dep10/library/book/service/BookService.java
@@ -1,6 +1,18 @@
 package lk.ijse.dep10.library.book.service;
 
+import lk.ijse.dep10.library.book.dto.BookDTO;
+
+import java.util.List;
+
 public interface BookService {
 
+    void saveBook(BookDTO book);
 
+    void updateBook(BookDTO book);
+
+    void deleteBook(String isbn);
+
+    BookDTO getBook(String isbn);
+
+    List<BookDTO> findBooks(String query);
 }
