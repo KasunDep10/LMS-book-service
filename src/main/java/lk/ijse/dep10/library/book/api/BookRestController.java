f09e763fc0adb3999184764fc8db96a2a54844d4
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-05 13:40:59 +0530

    Expose some web services and add Book entity

---
+++ src/main/java/lk/ijse/dep10/library/book/api/BookRestController.java
@@ -0,0 +1,34 @@
+package lk.ijse.dep10.library.book.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/books")
+@CrossOrigin
+public class BookRestController {
+
+    @PostMapping
+    public String saveBook(){
+        return "<h1>Save Book</h1>";
+    }
+
+    @PatchMapping("/{isbn}")
+    public String updateBook(){
+        return "<h1>Update Book</h1>";
+    }
+
+    @DeleteMapping("/{isbn}")
+    public String removeBook(){
+        return "<h1>Remove Book</h1>";
+    }
+
+    @GetMapping("/{isbn}")
+    public String getBook(){
+        return "<h1>Get a Book</h1>";
+    }
+
+    @GetMapping
+    public String findBooks(){
+        return "<h1>Find Books</h1>";
+    }
+}
---
+++ src/main/java/lk/ijse/dep10/library/book/entity/Book.java
@@ -0,0 +1,8 @@
+package lk.ijse.dep10.library.book.entity;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private int copies;
+}
