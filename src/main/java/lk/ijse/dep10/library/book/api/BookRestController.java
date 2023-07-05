9819b8f1f44dca259ebd41ce7218e8dc67635007
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-05 15:08:30 +0530

    Implement the api layer and apply validation

--- src/main/java/lk/ijse/dep10/library/book/api/BookRestController.java
+++ src/main/java/lk/ijse/dep10/library/book/api/BookRestController.java
@@ -1,34 +1,51 @@
 package lk.ijse.dep10.library.book.api;
 
+import lk.ijse.dep10.library.book.dto.BookDTO;
+import lk.ijse.dep10.library.book.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/books")
 @CrossOrigin
 public class BookRestController {
 
-    @PostMapping
-    public String saveBook(){
-        return "<h1>Save Book</h1>";
+    private final BookService bookService;
+
+    public BookRestController(BookService bookService) {
+        this.bookService = bookService;
     }
 
-    @PatchMapping("/{isbn}")
-    public String updateBook(){
-        return "<h1>Update Book</h1>";
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveBook(@RequestBody @Validated BookDTO bookDTO){
+        bookService.saveBook(bookDTO);
+    }
+
+    @PatchMapping(value = "/{isbn}", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void updateBook(@PathVariable String isbn, @RequestBody @Validated BookDTO bookDTO){
+        bookDTO.setIsbn(isbn);
+        bookService.updateBook(bookDTO);
     }
 
     @DeleteMapping("/{isbn}")
-    public String removeBook(){
-        return "<h1>Remove Book</h1>";
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeBook(@PathVariable String isbn){
+        bookService.deleteBook(isbn);
     }
 
     @GetMapping("/{isbn}")
-    public String getBook(){
-        return "<h1>Get a Book</h1>";
+    public BookDTO getBook(@PathVariable String isbn){
+        return bookService.getBook(isbn);
     }
 
     @GetMapping
-    public String findBooks(){
-        return "<h1>Find Books</h1>";
+    public List<BookDTO> findBooks(@RequestParam(required = false) String query){
+        if(query == null) query = "";
+        return bookService.findBooks(query);
     }
 }
--- src/main/java/lk/ijse/dep10/library/book/dto/BookDTO.java
+++ src/main/java/lk/ijse/dep10/library/book/dto/BookDTO.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.PositiveOrZero;
 import java.io.Serializable;
 
 @Data
@@ -12,8 +15,14 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Builder
 public class BookDTO implements Serializable {
+    @NotBlank(message = "ISBN can't be empty")
     private String isbn;
+    @NotBlank(message = "Title can't be empty")
     private String title;
+    @NotBlank(message = "Author can't be empty")
+    @Pattern(regexp = "^[A-Za-z ]+$", message = "Invalid author name")
     private String author;
+    @NotBlank(message = "Copies can't be null")
+    @PositiveOrZero(message = "Copies can't be negative")
     private Integer copies;
 }
